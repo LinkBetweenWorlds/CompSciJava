19476a36c9544736f287d4ecf34e38c76d9ae1ad
LinkBetweenWorlds <William03lawson@gmail.com>
2023-02-10 07:13:38 -0800

    Ch3

--- Ch3/src/HW3_2.java
+++ Ch3/src/HW3_2.java
@@ -1,8 +1,45 @@
+/*
+CIST 004A1 Spring 2023
+HW Week 2 Problem 8
+Description: This program calculates the shipping cost depending on the weight.
+Input: Weight
+Output: Shipping cost
+Student: William Lawson
+Known buds: None
+Date: 2/10/2023
+ */
+
 import java.util.Scanner;
 
 public class HW3_2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Please enter the weight of your package:");
+        double weight = input.nextDouble();
+
+        if (weight > 0) {
+            if (weight <= 1) {
+                System.out.println("Shipping Cost: $" + 3.5);
+            } else if (weight <= 3) {
+                System.out.println("Shipping Cost: $" + 5.5);
+            } else if (weight <= 10) {
+                System.out.println("Shipping Cost: $" + 8.5);
+            } else if (weight <= 20) {
+                System.out.println("Shipping Cost: $" + 10.5);
+            } else {
+                System.out.println("Package is too heavy too ship.");
+            }
+        } else {
+            System.out.println("Please enter a weight greater than 0.");
+        }
     }
 }
+
+/*
+Console Input:
+15
+
+Console Output:
+Shipping Cost: $10.5
+ */
--- Ch3/src/HW3_3.java
+++ Ch3/src/HW3_3.java
@@ -1,5 +1,42 @@
+/*
+CIST 004A1 Spring 2023
+HW Week 2 Problem 9
+Description: This program generates a random card.
+Input: None
+Output: Card
+Student: William Lawson
+Known buds: None
+Date: 2/10/2023
+ */
+
 public class HW3_3 {
     public static void main(String[] args) {
+        int n = (int) (Math.random() * 52);
 
+        int suite = n / 13;
+        int rank = n % 13;
+
+        String house = switch (suite) {
+            case 0 -> "Clubs";
+            case 1 -> "Diamonds";
+            case 2 -> "Hearts";
+            case 3 -> "Spades";
+            default -> "";
+        };
+
+        String num = switch (rank) {
+            case 0 -> "Ace";
+            case 10 -> "Jack";
+            case 11 -> "Queen";
+            case 12 -> "King";
+            default -> Integer.toString(rank + 1);
+        };
+
+        System.out.println("The card you picked is " + num + " of " + house + ".");
     }
 }
+
+/*
+Console Output:
+The card you picked is 5 of Hearts.
+ */
