b4fc0c25d4703b254c1ad8a2997d8d03cdb76501
LinkBetweenWorlds <William03lawson@gmail.com>
2023-02-10 08:07:36 -0800

    HW4_2

--- Ch4/src/HW4_2.java
+++ Ch4/src/HW4_2.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class HW4_2 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter a SSN(DDD-DD-DDDD)");
+        String SSN = input.nextLine();
+        SSN = SSN.trim();
+
+        if (SSN.length() == 11) {
+        } else {
+            System.out.println(SSN + " is a invalid SSN.");
+        }
     }
 }
